23bed98cb35f67ca69661e0f14cb5a024f3dd302
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-30 18:50:40 +0530

    feat: Add useFetchAlbumsQuery hook for fetching albums
    This commit adds the `useFetchAlbumsQuery` hook to the `albumsApi` module. The hook is used to fetch albums for a specific user. It makes a GET request to the `/albums` endpoint with the `userId` as a query parameter. This hook will be used in the `AlbumsList` component to fetch albums for a given user.

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// fetchBaseQuery function gives us a pre-configured version of fetch
+// rtk-query uses fetch
+
+export const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchAlbumsQuery } = albumsApi;
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { usersReducer } from './slices/usersSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
+import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
 
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUser';
+setupListeners(store.dispatch);
+
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
+export * from "./thunks/addUser";
+export * from "./thunks/fetchUsers";
+export * from "./thunks/removeUser";
