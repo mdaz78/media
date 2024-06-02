c53969e1dd9e747079bba63a88d12906f86d1c5e
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-02 12:41:17 +0530

    feat: Add photosApi to store and update photos

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,51 @@
+import { faker } from "@faker-js/faker";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const photosApi = createApi({
+  reducerPath: "photos",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchPhotos: builder.query({
+        providesTags: [],
+        query: (album) => {
+          return {
+            method: "GET",
+            url: "/photos",
+            params: {
+              albumId: album.id,
+            },
+          };
+        },
+      }),
+      addPhotos: builder.mutation({
+        query: (album) => {
+          return {
+            method: "POST",
+            url: "/photos",
+            params: {
+              albumId: album.id,
+              url: faker.image.abstract(150, 150, true),
+            },
+          };
+        },
+      }),
+      removePhotos: builder.mutation({
+        query: (photo) => {
+          return {
+            method: "DELETE",
+            url: `/photos/${photo.id}`,
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const {
+  useFetchPhotosQuery,
+  useAddPhotosMutation,
+  useRemovePhotosMutation,
+} = photosApi;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 
@@ -20,6 +24,12 @@ export {
   useDeleteAlbumMutation,
   useFetchAlbumsQuery,
 } from "./apis/albumsApi";
+export {
+  useAddPhotosMutation,
+  useFetchPhotosQuery,
+  useRemovePhotosMutation,
+} from "./apis/photosApi";
+
 export * from "./thunks/addUser";
 export * from "./thunks/fetchUsers";
 export * from "./thunks/removeUser";
